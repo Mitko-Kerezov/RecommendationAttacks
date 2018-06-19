40910c866f2d31211b9272d753a4341db052ce32
Novkirishki <novkirishki94@gmail.com>
2018-06-19 12:53:58 +0300

    Added generated dataset

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -1,17 +1,24 @@
 import pandas as pd
 from surprise import Reader, Dataset, NMF, SVD, evaluate
 
-reader = Reader(rating_scale=(0.5, 5.0), sep=',')
+reader = Reader(rating_scale=(1.0, 5.0), sep=',')
 data = Dataset.load_from_file("ratings.csv", reader)
 trainset = data.build_full_trainset()
 
 # svd
 algo = SVD()
-algo.train(trainset)
+algo.fit(trainset)
 
-uid = "3"
-pred = algo.predict(uid=uid, iid="", r_ui=1287, verbose=True)
-print(pred)
+# First user group are drama lovers
+users = [(1,200), (201,400), (401,600), (601,800), (801,1000)]
+
+for user_group in users:
+    group_avg = 0
+    for user in range(user_group[0], user_group[1] + 1):
+        # Movie with ID=1 is Titanic
+        pred = algo.predict(uid=str(user), iid="1", r_ui=None, verbose=False)
+        group_avg += pred.est
+    print(group_avg/200)
 
 # evaluate(algo, data, measures=['RMSE'])
 
