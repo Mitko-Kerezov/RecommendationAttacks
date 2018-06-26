4581d6b4faabbbdd228c546329c63f4fbf4527c9
Dimitar Kerezov <dimitar.kerezov@telerik.com>
2018-06-26 21:18:39 +0300

    Remove stopwords and punctuation

--- content_based_filtering.py
+++ content_based_filtering.py
@@ -1,7 +1,8 @@
 import gensim
 import os
 import json
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
 
 # class SentencesCollector:
 #     def __init__(self, dirname):
@@ -20,6 +21,12 @@ from nltk.tokenize import word_tokenize
 # print("vector of the word 'deadly'" % model['deadly'])
 # print("model.similarity('game', 'battle') is %s" % model.similarity('game', 'battle'))
 
+tokenizer = RegexpTokenizer(r'\w+')
+def getWords(plot):
+    result = tokenizer.tokenize(plot)
+    result = filter(lambda w: w not in stopwords.words('english'), result)
+    return list(map(lambda w: w.lower(), result))
+
 dirName = "data"
 queryStr = ""
 plots = []
@@ -28,12 +35,11 @@ for fname in os.listdir(dirName):
     fhandle = open(fpath, 'r', encoding="utf8")
     contents = json.load(fhandle)
     text = contents['Plot']
-    print(fname)
-    if fname in [str(ind) + ".json" for ind in range(21, 30)]:
+    if fname in ["0" + str(ind) + ".json" for ind in range(61, 66)]:
         queryStr += text
     plots.append(text)
 
-gen_docs = [[w.lower() for w in word_tokenize(plot)] for plot in plots]
+gen_docs = list(map(getWords, plots))
 
 dictionary = gensim.corpora.Dictionary(gen_docs)
 
@@ -46,10 +52,12 @@ sims = gensim.similarities.SparseMatrixSimilarity(
     tf_idf[corpus], num_features=len(dictionary))
 
 print(queryStr)
-query_doc = [w.lower() for w in word_tokenize(queryStr)]
+query_doc = getWords(queryStr)
 query_doc_bow = dictionary.doc2bow(query_doc)
+# query_doc_bow = [(2158, 5), (2194, 1), (2196, 3), (2228, 2), (2198, 2)]
 query_doc_tf_idf = tf_idf[query_doc_bow]
 
 sim_matrix = sims[query_doc_tf_idf]
-print(sorted(range(len(sim_matrix)), key=lambda i: sim_matrix[i])[-5:])
-# print(a)
+print(sim_matrix)
+sortedM = list(reversed(sorted(range(len(sim_matrix)), key=lambda i: sim_matrix[i])))
+print(sortedM)
